22c626242103ae16bb4643562c72df944627f286
Yayun Zhao <yayun.y.zhao@pwc.com>
2017-05-31 10:57:22 +0800

    added one image

--- dist/index.js
+++ dist/index.js
@@ -15,17 +15,9 @@ var _DepartmentComponent2 = _interopRequireDefault(_DepartmentComponent);
 
 require('./css/root.css');
 
-var _reactTapEventPlugin = require('react-tap-event-plugin');
-
-var _reactTapEventPlugin2 = _interopRequireDefault(_reactTapEventPlugin);
-
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-// Demo component
+exports.StoreComponent = _StoreComponent2.default;
+exports.DepartmentComponent = _DepartmentComponent2.default; // Demo component
 // this is only example component
 // you can find tests in __test__ folder
-
-(0, _reactTapEventPlugin2.default)();
-
-exports.StoreComponent = _StoreComponent2.default;
-exports.DepartmentComponent = _DepartmentComponent2.default;
--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
     return (
       <div className="App">
         
-	        <DepartmentComponent />
+	        <StoreComponent />
 	  </div>
     );
   }
